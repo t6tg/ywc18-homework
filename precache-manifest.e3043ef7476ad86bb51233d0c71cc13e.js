007f3caa8767cff8c97a05dc64001ca3dfdaf207
t6tg <t6tg@users.noreply.github.com>
2020-11-18 10:15:43 +0000

    deploy: 7ff12df38ea0a347737a7ce721967d211ad96030

---
+++ precache-manifest.e3043ef7476ad86bb51233d0c71cc13e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49526a04df9ec0aa4c94",
+    "url": "/ywc18-homework/css/app.4e486f46.css"
+  },
+  {
+    "revision": "c3a9b276c341f38b93f7076544012d93",
+    "url": "/ywc18-homework/favicon.png"
+  },
+  {
+    "revision": "f8cbd8d669126c679b581155caa95ad4",
+    "url": "/ywc18-homework/img/bg.f8cbd8d6.png"
+  },
+  {
+    "revision": "c3a9b276c341f38b93f7076544012d93",
+    "url": "/ywc18-homework/img/half-min.c3a9b276.png"
+  },
+  {
+    "revision": "c1df95df241169c891ba1cdb56601eaf",
+    "url": "/ywc18-homework/img/halfhalf-logo.c1df95df.png"
+  },
+  {
+    "revision": "866a0544d5615a6dbda84223bb7068ca",
+    "url": "/ywc18-homework/index.html"
+  },
+  {
+    "revision": "49526a04df9ec0aa4c94",
+    "url": "/ywc18-homework/js/app.c0af6fa1.js"
+  },
+  {
+    "revision": "90388f266c16ef92f861",
+    "url": "/ywc18-homework/js/chunk-vendors.c3a51f2b.js"
+  },
+  {
+    "revision": "5ae81fe5a8cbb09ce5c504db008d9c60",
+    "url": "/ywc18-homework/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ywc18-homework/robots.txt"
+  }
+]);
